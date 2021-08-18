95caa1cb638479ace124120486c8ad5cb1244766
Zimon Kuhs <zmk.professional@gmail.com>
2021-08-18 18:32:31 +0200

    feat(Language): Add scramble functions.

--- src/language/words.py
+++ src/language/words.py
@@ -5,10 +5,13 @@
     @date:    2021-08-18.
 """
 
+import random
+
+
 def scramble(word) :
     function = {
-        "string" : scrambleString,
-        "array"  : scrambleArray
+            str : scrambleString,
+            list  : scrambleArray
         }[type(word)]
 
     if function is None :
@@ -16,8 +19,28 @@ def scramble(word) :
 
     return function(word)
 
-def scrambleArray(word) :
-    return None
+"""
+    Creates a new list containing the same elements as in the input, but scrambled.
 
+    @param array   The array of objects to scramble.
+    @return        The randomized list.
+"""
+def scrambleArray(array) :
+    chars = array.copy()
+
+    result = []
+    while chars :
+        result.append(chars[random(0, len(chars))])
+
+    return result
+
+"""
+    Returns an anagram of a string.
+
+    TODO: Probably not too optimized...
+
+    @param word    The original string to scramble.
+    @return        A randomized anagram of the input.
+"""
 def scrambleString(word) :
-    return None
+    return "".join(scrambleArray([char for char in word]))
