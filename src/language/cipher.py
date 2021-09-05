05a3d3e12100b149e244af53a3049bbb9ae169fa
Zimon Kuhs <zmk.professional@gmail.com>
2021-09-05 12:43:25 +0200

    feat(Cipher): Push local changes to branch.

--- src/language/cipher.py
+++ src/language/cipher.py
@@ -5,8 +5,73 @@
     @date:    2021-08-18.
 """
 
+import sys
+
+from words import scramble
+
+"""
+    Ciphers a string using letter displacemenet via a number.
+
+    TODO: Currently only operates with capital letters.
+
+    @param word     The string to cipher.
+    @return         The randomized list.
+"""
 def caesarCipher(word, key) :
     if not isinstance(word, str) :
         raise Exception("Can only cipher an instance of string (got %s)." % type(word))
 
-    return "".join([char(int(char) + key) for char in word])
+    return "".join([char(64 + (int(char) + key) % 25) for char in word])
+
+"""
+    Ciphers a string using a custom conversion function.
+
+    @param word             The string to cipher.
+    @param charFunction     The conversion function.
+    @param keys             The list of keys to use for each character, with the input function.
+"""
+def customCipher(word, charFunction, keys) :
+    if not isinstance(word, str) :
+        raise Exception("Can only cipher an instance of string (got %s)." % type(word))
+
+    if not isinstance(charFunction, function) :
+        raise Exception("Can only cipher using a character displacement function (got %s)." % type(charFunction))
+
+    if not isinstance(charFunction, list) or not keys:
+        raise Exception("A dict of keys is required in order to cipher a string (got %s)." % keys)
+
+    return "".join([charFunction(char, keys) for char in word])
+
+
+""" ************************** """
+""" Temporary ciphering place. """
+""" ************************** """
+
+if __name__ == "__main__" :
+    if len(sys.argv) != 2 :
+        print("Usage: python cipher.py <word>")
+        sys.exit(1)
+
+    key1 = 13
+    key2 = 2
+    key3 = -1
+
+    scrambled = scramble(sys.argv[1])
+    newWord = ""
+
+    for i in range(0, len(scrambled)) :
+        digit = ord(scrambled[i]) - 64
+        newDigit = digit + (key1 + pow(key2, i)) * pow(key3, i)
+        newAscii = 65 + newDigit % 25
+        print(newAscii)
+        newWord += chr(newAscii)
+
+    oldWord = ""
+    for i in range(0, len(scrambled)) :
+        ascii = ord(scrambled[i])
+        oldChar = chr(65 + (ascii - (key1 + pow(key2, i)) * pow(key3, i)) % 25)
+        oldWord += oldChar
+
+    print(newWord)
+    print(oldWord)
+    sys.exit(0)
--- src/language/words.py
+++ src/language/words.py
@@ -5,9 +5,16 @@
     @date:    2021-08-18.
 """
 
-import random
+from random import *
 
+"""
+    Scrambles an object (currently only lists or strings).
 
+    TODO: Extend to cover other types?
+
+    @param word     The object to scramble.
+    @return         The scrambled object.
+"""
 def scramble(word) :
     function = {
             str : scrambleString,
@@ -22,15 +29,15 @@ def scramble(word) :
 """
     Creates a new list containing the same elements as in the input, but scrambled.
 
-    @param array   The array of objects to scramble.
-    @return        The randomized list.
+    @param array    The array of objects to scramble.
+    @return         The randomized list.
 """
 def scrambleArray(array) :
     chars = array.copy()
 
     result = []
     while chars :
-        result.append(chars[random(0, len(chars))])
+        result.append(chars.pop(randint(0, len(chars) - 1)))
 
     return result
 
@@ -39,8 +46,8 @@ def scrambleArray(array) :
 
     TODO: Probably not too optimized...
 
-    @param word    The original string to scramble.
-    @return        A randomized anagram of the input.
+    @param word     The original string to scramble.
+    @return         A randomized anagram of the input.
 """
 def scrambleString(word) :
     return "".join(scrambleArray([char for char in word]))
